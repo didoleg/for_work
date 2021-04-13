dea4e1da2227ae7a75e24ac0110f111c2e96f26e
oleg <didoleg@mail.ru>
2021-04-13 17:55:43 +0700

    add cgange task_4_2

--- dz_4/task_4_2.py
+++ dz_4/task_4_2.py
@@ -2,11 +2,13 @@ import requests
 from lxml import etree
 
 xml_response = etree.fromstring(requests.get("http://www.cbr.ru/scripts/XML_daily.asp").text.encode("1251"))
-curs = xml_response.find("Valute[@ID='R01200']/Value").text
+print(type(xml_response))
+curs = xml_response.find("Valute[@ID='R01090B']/Value").text
 
-print(f"Один Гонконгский доллар равен {curs} рублей")
+print(f"Один Белорусский рубль {curs} рублей")
 #print(response_1.headers['Content-Type'])
 
 if __name__ == '__main__':
     pass
-https://pythonru.com/biblioteki/prodvinutoe-rukovodstvo-po-biblioteke-python-requests
+# https://pythonru.com/biblioteki/prodvinutoe-rukovodstvo-po-biblioteke-python-requests
+# https://www.youtube.com/watch?v=IK448-nC8yU
